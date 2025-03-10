671ad3182a184faf28aa7b000a8bfc21f76615aa
Nicolas Macias Duque <nicolas.macias@s4ds.com>
2025-03-10 17:03:46 -0500

    Se suben los ajustes de la 3era sesion de estudio

---
+++ ReactAppStyles/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Button from './components/button/button'
+
+function App() {
+
+  return (
+    <>
+     <Button blue>hola</Button>
+     <Button>Gris</Button>
+
+    </>
+  )
+}
+
+export default App
---
+++ ReactAppStyles/src/components/card/card.tsx
@@ -0,0 +1,20 @@
+//import "./styles.css"
+//import styles from "./card.module.scss"
+
+const Card = () =>{
+
+    const textStyles: React.CSSProperties = {
+        color : "blue",
+        fontSize: "2rem",
+        textAlign:"center"
+    }
+
+    return(
+        <div style={textStyles}>
+            esto es una card LOL
+        </div>
+    )
+
+}
+
+export default Card;
